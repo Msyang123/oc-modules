bb75f6342f2b72cd3324626b7173a2a1f918d85b
yijun <290828414@qq.com>
2018-09-21 10:04:21 +0800

    配送服务调用订单服务feign调整

--- delivery-service/src/main/java/com/lhiot/oc/delivery/feign/OrderService.java
+++ delivery-service/src/main/java/com/lhiot/oc/delivery/feign/OrderService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@FeignClient("payment-order-service-v1-0")
+@FeignClient("order-service-v1-0")
 @Component
 public interface OrderService {
 
