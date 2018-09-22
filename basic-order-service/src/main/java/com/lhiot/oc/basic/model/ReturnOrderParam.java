fed242c5fef434f6628b85fdded162edcd94864e
yijun <290828414@qq.com>
2018-09-22 12:20:50 +0800

    订单取消 退货处理

---
+++ basic-order-service/src/main/java/com/lhiot/oc/basic/model/ReturnOrderParam.java
@@ -0,0 +1,22 @@
+package com.lhiot.oc.basic.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+@ApiModel
+/**
+ * 退款商品参数
+ */
+public class ReturnOrderParam {
+
+    @ApiModelProperty(notes = "退货原因", dataType = "String")
+    private String returnReason;
+
+    @ApiModelProperty(notes = "订单商品ID（一个或者多个）多个以,分割", dataType = "String")
+    private String orderProductIds;
+}
---
+++ basic-order-service/src/main/java/com/lhiot/oc/basic/service/OrderFlowService.java
@@ -0,0 +1,37 @@
+package com.lhiot.oc.basic.service;
+
+import com.lhiot.oc.basic.mapper.OrderFlowMapper;
+import com.lhiot.oc.basic.model.BaseOrderInfo;
+import com.lhiot.oc.basic.model.OrderFlow;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
+@Slf4j
+@Transactional
+public class OrderFlowService {
+    private OrderFlowMapper orderFlowMapper;
+
+    public OrderFlowService(OrderFlowMapper orderFlowMapper) {
+        this.orderFlowMapper = orderFlowMapper;
+    }
+
+    public int create(BaseOrderInfo searchBaseOrderInfo,BaseOrderInfo baseOrderInfo){
+
+        OrderFlow orderFlow = new OrderFlow();
+        orderFlow.setOrderId(baseOrderInfo.getId());
+        orderFlow.setStatus(baseOrderInfo.getStatus());
+        orderFlow.setPreStatus(searchBaseOrderInfo.getStatus());
+        orderFlow.setCreateAt(new Timestamp(System.currentTimeMillis()));
+        return orderFlowMapper.create(orderFlow);
+    }
+
+    //根据订单id查询
+    public List<OrderFlow> flowByOrderId(Long orderId){
+        return orderFlowMapper.flowByOrderId(orderId);
+    }
+}
